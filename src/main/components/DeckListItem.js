b34a5c06c0d9ee418fad84dcb0df54dc78f274fd
EvanFung <evanfungv@gmail.com>
2018-03-22 02:05:36 +0800

    feat: add decklistitem ui

---
+++ src/main/components/DeckListItem.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+
+export default function DeckListItem({ deck }) {
+  return (
+    <View>
+      <TouchableOpacity
+        key={key}
+        onPress={() => console.log(`clicked ${deck.title}`)}
+      >
+        <Text>{deck.title}</Text>
+        <Text>{deck.questions.length} questions.</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
