64608f3bfc10fb0c145e0940dc750bcaef8a0498
EvanFung <evanfungv@gmail.com>
2018-03-23 01:45:57 +0800

    feat: add getRandomInt function

--- src/main/components/DeckQuiz.js
+++ src/main/components/DeckQuiz.js
@@ -2,7 +2,13 @@ import React from "react"
 import { connect } from "react-redux"
 import { View, Text, StyleSheet } from "react-native"
 
+const NUMBER_QUESTIONS = 10
 class DeckQuiz extends React.Component {
+  state = {
+    score: 0,
+    cardCounter: 0,
+    currentCard: null
+  }
   //override the default navigationOptions.
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state
@@ -10,13 +16,23 @@ class DeckQuiz extends React.Component {
       title: `Deck Quiz`
     }
   }
+
   render() {
+    const { score, cardCounter, currentCard } = this.state
     return (
       <View>
-        <Text>Deck Quiz</Text>
+        <Text>
+          Progress: {cardCounter} / {NUMBER_QUESTIONS}
+        </Text>
+        <Text>Current Score: {score}</Text>
       </View>
     )
   }
 }
-
+function mapState(state, { navigation }) {
+  const { deckId } = navigation.state.params
+  return {
+    deck: state.decks[deckId]
+  }
+}
 export default DeckQuiz
--- src/main/utils/utils.js
+++ src/main/utils/utils.js
@@ -1,9 +1,15 @@
-import uuidv4 from 'uuid/v4'
-import { Alert } from "react-native";
+import uuidv4 from "uuid/v4"
+import { Alert } from "react-native"
 export function generateRandomId() {
-    return uuidv4();
+  return uuidv4()
 }
 
 export const showAlert = () => {
-    Alert.alert('Enter a value');
+  Alert.alert("Enter a value")
+}
+
+export function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
 }
