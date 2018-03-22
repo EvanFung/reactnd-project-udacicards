cb0857d1ccd95b93de23e92087bc5d55ebeb1851
EvanFung <evanfungv@gmail.com>
2018-03-23 02:28:57 +0800

    feat: add quiz view logic

--- src/main/components/DeckQuiz.js
+++ src/main/components/DeckQuiz.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { connect } from "react-redux"
 import { View, Text, StyleSheet } from "react-native"
-
+import { getRandomInt } from "../utils/utils"
+import Button from "./TouchableButton"
 const NUMBER_QUESTIONS = 10
 class DeckQuiz extends React.Component {
   state = {
@@ -16,15 +17,69 @@ class DeckQuiz extends React.Component {
       title: `Deck Quiz`
     }
   }
+  //randomly pick up a question from deck.
+  getRandomCard = () => {
+    const questions = this.props.deck.questions
+    const randomIndex = getRandomInt(0, questions.length)
+    this.setState({
+      currentCard: questions[randomIndex],
+      cardCounter: this.state.cardCounter + 1
+    })
+  }
+
+  componentWillMount() {
+    this.getRandomCard()
+  }
+
+  onSubmitAnswer(isTrue) {
+    const questions = this.props.deck.questions
+    //update the score if selection is true
+    if (isTrue) {
+      this.setState({
+          score: this.state.score + 1
+      })
+    }
+
+    if (this.state.cardCounter <= NUMBER_QUESTIONS) {
+      this.getRandomCard()
+      return
+    }
+  }
+
+  initialQuiz = () => {
+    this.setState(
+      {
+        score: 0,
+        cardCounter: 0,
+        currentCard: null
+      },
+      this.getRandomCard
+    )
+  }
 
   render() {
     const { score, cardCounter, currentCard } = this.state
+    if (!currentCard) {
+      return null
+    }
+    if (cardCounter === NUMBER_QUESTIONS) {
+      return <Button onPress={this.initialQuiz}>GAME COMPLETED</Button>
+    }
     return (
       <View>
         <Text>
           Progress: {cardCounter} / {NUMBER_QUESTIONS}
         </Text>
         <Text>Current Score: {score}</Text>
+        <Text>{currentCard.question}</Text>
+        {currentCard.answers.map((answer, index) => (
+          <Button
+            key={index}
+            onPress={() => this.onSubmitAnswer(answer.isTrue)}
+          >
+            {answer.text}
+          </Button>
+        ))}
       </View>
     )
   }
@@ -35,4 +90,4 @@ function mapState(state, { navigation }) {
     deck: state.decks[deckId]
   }
 }
-export default DeckQuiz
+export default connect(mapState)(DeckQuiz)
--- src/main/utils/utils.js
+++ src/main/utils/utils.js
@@ -13,3 +13,15 @@ export function getRandomInt(min, max) {
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min)) + min
 }
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+export function shuffleArray(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
